3f5ada8714f7e3b1a6d35c20d8e60205bb6ab93f
Diona Moss <123892824+D-Moss@users.noreply.github.com>
2026-05-09 22:25:14 -0400

    Add files via upload
    created a site called finflow. completed the full html, css, and js for the homepage.

---
+++ index.js
@@ -0,0 +1,61 @@
+// Firebase Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/11.7.3/firebase-auth.js";
+
+
+// Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyDKNumgI1MvLAdPqD4u3Qah_SbWWAbfwR4",
+    authDomain: "finflow-dcab6.firebaseapp.com",
+    projectId: "finflow-dcab6",
+    storageBucket: "finflow-dcab6.firebasestorage.app",
+    messagingSenderId: "536136161454",
+    appId: "1:536136161454:web:885b77f96109d30850f53f",
+    measurementId: "G-MDY4MDRC16"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+// Sign Up
+document.getElementById("signupBtn").addEventListener("click", () => {
+
+    const email = prompt("Enter email:");
+    const password = prompt("Enter password:");
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            alert("Account created!");
+            console.log(userCredential.user);
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+
+});
+
+
+// Login
+document.getElementById("loginBtn").addEventListener("click", () => {
+
+    const email = prompt("Enter email:");
+    const password = prompt("Enter password:");
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            alert("Logged in!");
+            console.log(userCredential.user);
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+
+});
